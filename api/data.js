f46d7e0d1762d2d99148750e2c6294940bdd3c58
michael121yang <my435@cornell.edu>
2017-03-16 22:44:46 -0400

    removed stray s

--- api/data.js
+++ api/data.js
@@ -47,7 +47,7 @@ const get_callbacks = competitors => competitors
   "id" : i,
   "timestamp" : randomDate({year: 2017}).toDateString(),
   "judge_id" : randomId(JUDGES),
-  "Lead Competitor number" : c["Lead Number"s],
+  "Lead Competitor number" : c["Lead Number"],
   "round_id" : randomId(ROUNDS),
 }))
 
