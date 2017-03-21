bfac113d668df44a13c924c6a645bf4e44d9fdd6
michael121yang <my435@cornell.edu>
2017-03-21 14:07:55 -0400

    fixes

--- src/common/Routes.jsx
+++ src/common/Routes.jsx
@@ -10,6 +10,7 @@ import EventRegistration    from '../PageEventRegistration.jsx'
 import EditSchedule         from '../PageEditSchedule.jsx'
 import RunCompetition       from '../PageRunCompetition.jsx'
 import CompetitionHomeAdmin from '../PageCompetitionHomeAdmin.jsx'
+import CompetitorsList      from '../PageCompetitorList.jsx'
 import EditProfile          from '../PageEditProfile.jsx'
 
 const routes = {
@@ -18,7 +19,7 @@ const routes = {
   'competition/:competition_id/editschedule'      : EditSchedule,
   'competition/:competition_id/run'               : RunCompetition,
   'competition/:competition_id/:competitor_id'    : CompetitionPage,
-  'competition/:competition_id/competitorlist'    : CompetitorsList,
+  'competition/:competition_id/competitorslist'   : CompetitorsList,
   'competitions'                                  : CompetitionListPage,
   'admin/competition/:competition_id'             : CompetitionHomeAdmin,
   'editprofile'                                   : EditProfile,
