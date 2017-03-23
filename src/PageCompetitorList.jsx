3a230dd3f66fb8f5075f3947c7d7e6ea6a94c96a
michael121yang <my435@cornell.edu>
2017-03-23 18:58:38 -0400

    A bunch of competitor list attempts

--- src/PageCompetitorList.jsx
+++ src/PageCompetitorList.jsx
@@ -6,11 +6,12 @@ import {Button, IconButton } from 'react-toolbox/lib/button';
 import { Snackbar } from 'react-toolbox/lib/snackbar';
 import lib from './common/lib.js'
 
+
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 
 import Page from './Page.jsx'
-import DragAndDropTable from './PageCompetitorList/competitors.jsx';
+import SearchTable from './PageCompetitorList/competitors.jsx';
 import style from './style.css';
 
 // competition/:competition_id/competitorlist
@@ -20,21 +21,20 @@ class CompetitorsList extends React.Component {
   return (
     <Page ref="page">
       <div id={style.titleContainer}>
-        <h1>Competitors</h1>
+        <h1>List of Competitors</h1>
         <div id={style.buttonsContainer}>
-          <div id={style.saveChanges}>Save Changes</div>
-          <div id={style.cancelChanges}>Cancel</div>
+          <div id={style.saveChanges}>See Organizations</div>
         </div>
       </div>
       <div id={style.dragAndDropWrapper}>
         <div id={style.dragAndDropWrapperTopBar}>
-          <div id={style.dragAndDropTitle}>Rounds</div>
+          <div id={style.dragAndDropTitle}>Competitors</div>
           <div id={style.dragAndDropAutosort}>
             <div>Autosort</div>
           </div>
         </div>
         <div id={style.dragAndDropTableScroller}>
-          <DragAndDropTable />
+          <SearchTable />
         </div>
       </div>
     </Page>
