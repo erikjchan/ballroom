c18fd92e29702e4113f7a102d51a4a5840e44882
Kevin <mr.kev@me.com>
2017-05-10 01:23:58 -0400

    SRC

--- src/PageCompetitorList/competitors.jsx
+++ src/PageCompetitorList/competitors.jsx
@@ -81,15 +81,15 @@ export default class CompetitorList extends React.Component {
       	{
             cell: {
       		    formatters: [
-                    (value, { rowData }) => (
-                        <div>
-                            <button
-                              className={style.editBtns}
-                              onClick={()=>{ browserHistory.push(`/competition/${1}/seecompetitor/${rowData.id}`) }}>
-                                Edit/See More
-                            </button>
-      			            </div>
-      		        )
+                  (value, { rowData }) => (
+                    <div>
+                      <button
+                        className={style.editBtns}
+                        onClick={()=>{ browserHistory.push(`/competition/${this.props.selected.competition}/seecompetitor/${rowData.id}`) }}>
+                          Edit/See More
+                      </button>
+      			        </div>
+      		      )
       		    ]
       	    },
       		width: 100
@@ -180,7 +180,7 @@ export default class CompetitorList extends React.Component {
                           <div>
                             <button
                               className={style.editBtns}
-                              onClick={()=>{ browserHistory.push(`/competition/${1}/seecompetitor/${rowData.id}`) }}>
+                              onClick={()=>{ browserHistory.push(`/competition/${this.props.selected.competition}/seecompetitor/${rowData.id}`) }}>
                                 Edit/See More
                             </button>
       			               </div>
@@ -199,20 +199,18 @@ export default class CompetitorList extends React.Component {
   }
 
   componentDidMount() {
-    fetch("/api/competition/1/competitors")
-	      .then(response => response.json())
+    this.props.api.get(`/api/competition/${this.props.selected.competition}/competitors`)
 		  .then(json => {
-            console.log(json);
-            this.rows = json;
-            for (let i = 0; i < this.rows.length; i++) {
-                  this.rows[i].amount = "$" + (this.rows[i].amount || 0);
-                  if (this.rows[i].paidwithaffiliation) {
-                          this.rows[i].paidwithaffiliation = "Yes";
-                  } else {
-                          this.rows[i].paidwithaffiliation = "No";
-                  }
-		        }
-		        this.setState({ rows: json, }); 
+        this.rows = json;
+        for (let i = 0; i < this.rows.length; i++) {
+              this.rows[i].amount = "$" + (this.rows[i].amount || 0);
+              if (this.rows[i].paidwithaffiliation) {
+                      this.rows[i].paidwithaffiliation = "Yes";
+              } else {
+                      this.rows[i].paidwithaffiliation = "No";
+              }
+		    }
+		    this.setState({ rows: json, }); 
 		 })
 		 .catch(err => alert(err));
   }
